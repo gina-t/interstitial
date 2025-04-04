217b1fca3366e1b0914a6ce635c7b0591d021256
Tao <ginadrcoder@gmail.com>
2025-04-04 19:18:43 +1100

    polish user interface

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -38,10 +38,10 @@ export default function Hero() {
           </p>
           <div className="mt-10 flex items-center gap-x-6">
             <Link
-              to="/login"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              to="/foodsearch"
+              className="rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Log in to use Nutrition API
+              Nutrition API
             </Link>
 
             <Link to='/learn-more' className='ml-4'>
